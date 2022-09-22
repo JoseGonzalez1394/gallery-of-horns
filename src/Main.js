11c740e185f306cf141ee5b9b4091bd85571fcb3
Jose A Gonzalez <JGman13@gmail.com>
2022-09-21 17:25:55 -1000

    grid responsive

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,39 @@
 import React from "react";
 import Data from "./Data.json";
 import HornedBeast from "./HornedBeast";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 class Main extends React.Component {
 
   render() {
     return (
       <>
-      {Data.map((value) => (
-        <HornedBeast
-        _id = {value._id}
-        image_url = {value.image_url}
-        title = {value.title}
-        description = {value.description}
-        keyword = {value.keyword}
-        horns = {value.horns}
-        favorites = {`Favorites: `}
-        key = {value._id}
-        />
-      ))}
+        <p className="fw-bold text-center border border-info .bg-secondary p-3 mb-2 bg-info text-dark">Please Click on Your Favorite Horned Beast! </p>
+        <div className="d-flex flex-wrap justify-content-center" >
+          <Container>
+            <Row>
+            {Data.map((value) => (
+                <Col sm>
+                  <HornedBeast
+                    _id={value._id}
+                    image_url={value.image_url}
+                    title={value.title}
+                    description={value.description}
+                    keyword={value.keyword}
+                    horns={value.horns}
+                    favorites={`Favorites: `}
+                    key={value._id}
+                  />
+                </Col>
+            ))}
+            </Row>
+          </Container>
+
+        </div>
       </>
     )
-    }
+  }
 }
 
 export default Main;
